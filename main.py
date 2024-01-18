977409b2e62262cb0394b28f5ade954ccae7822f
vokin23 <144451218+vokin23@users.noreply.github.com>
2024-01-18 10:20:34 +0300

    main.py

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import time
 
 
-def triplets(nums: list) -> list:
-    result = []
-    length = len(nums)
-    for i in range(length - 2):
-        for j in range(i + 1, length - 1):
-            for k in range(j + 1, length):
-                if nums[i] * nums[j] * nums[k] == 0 and i != j != k != i:
-                    result.append([nums[i], nums[j], nums[k]])
-    return result
+def find_triplets(nums):
+    result = set()
+    n = len(nums)
+    for i in range(n):
+        for j in range(i + 1, n):
+            for k in range(j + 1, n):
+                if nums[i] == 0 or nums[j] == 0 or nums[k] == 0:
+                    result.add(tuple(sorted([nums[i], nums[j], nums[k]])))
+    return [list(triplet) for triplet in result]
 
 
 if __name__ == "__main__":
     start_time = time.time()
     input_nums = [1, 2, 0, 3, 0, 4]
-    triplets_result = triplets(input_nums)
+    triplets_result = find_triplets(input_nums)
     print(triplets_result)
     end_time = time.time()
-    print(start_time, end_time)
     print(f"Время выполнения: {end_time - start_time} секунд")
